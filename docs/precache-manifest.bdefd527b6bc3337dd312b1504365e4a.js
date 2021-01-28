7af3562829fbcb6e957e23510e9c38fa84d80156
italo041 <italo_fernando11@hotmail.com>
2021-01-28 12:31:45 -0500

    fix

---
+++ docs/precache-manifest.bdefd527b6bc3337dd312b1504365e4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61c6d24e61fb8f9b183b7622e8aadee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7314518a1b612e7f1365",
+    "url": "/static/js/2.f8675c6c.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.f8675c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7153346e0747c41b7bfc",
+    "url": "/static/js/main.efd41030.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
