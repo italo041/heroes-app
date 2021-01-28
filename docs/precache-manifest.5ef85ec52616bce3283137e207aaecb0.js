bfb973222e19f31f902298c3f499994d514e0eb5
italo041 <italo_fernando11@hotmail.com>
2021-01-28 10:57:15 -0500

    Add docs

---
+++ docs/precache-manifest.5ef85ec52616bce3283137e207aaecb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d418c925fc254dde0a64c559deda8e52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7314518a1b612e7f1365",
+    "url": "/static/js/2.f8675c6c.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.f8675c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4c66693e34dced3ac86",
+    "url": "/static/js/main.72a75ea7.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
